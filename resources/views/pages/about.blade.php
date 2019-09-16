08d6d09e1f0abd26a442093f12a4ec1485e79690
Jonatas Oliveira (joliveirasi) <joliveirasi@indracompany.com>
2019-09-16 16:06:32 -0300

    Commit inicial - Projeto Blog - WEB

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>{{config('APP_NAME','LVBLOG')}}</title>  <!--pega o nome do .env-->
+        
+    </head>
+    <body>
+        <h1>ABOUT</h1>
+        <p>Esta é a pagina About</p>
+    </body>
+</html>
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>{{config('APP_NAME','LVBLOG')}}</title>  <!--pega o nome do .env-->
+        
+    </head>
+    <body>
+        <h1>Bem vindo ao LVBLOG</h1>
+        <p>Trabalho de PHP com Laravel, disciplina WEB</p>
+    </body>
+</html>
