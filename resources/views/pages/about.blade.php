869e28f5db04ce71de7597ea11aee2a4322be415
Jonatas Oliveira (joliveirasi) <joliveirasi@indracompany.com>
2019-09-17 10:40:23 -0300

    Criação de padrão de telas para ser extendido em outras telas

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>{{config('APP_NAME','LVBLOG')}}</title>  <!--pega o nome do .env-->
-        
-    </head>
-    <body>
-        <h1>ABOUT</h1>
-        <p>Esta é a pagina About</p>
-    </body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+    <h1>{{$title}}</h1>
+    <p>Esta é a pagina About</p>
+@endsection
