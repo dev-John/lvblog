fa813e582eb570572fc2d06b2d5dcc5ef9ba5e23
Jonatas Oliveira (joliveirasi) <joliveirasi@indracompany.com>
2019-09-24 16:45:50 -0300

    Scripts de insert

--- database/sqls/create_database.sql
+++ database/sqls/create_database.sql
@@ -4,3 +4,33 @@ use lvblog;
 
 
 select * from posts;
+
+insert into posts 
+(
+	title,
+    body,
+    created_at,
+    updated_at
+)
+values
+(
+	"Post 1",
+    "Corpo do post 1",
+    now(),
+    now()
+);
+
+insert into posts 
+(
+	title,
+    body,
+    created_at,
+    updated_at
+)
+values
+(
+	"Post 3",
+    "Corpo do post 3",
+    now(),
+    now()
+)
